e2dc8bd7d95662080540e1dd094722f11b6e9778
Kristofers Volkovs <kristofers.volkovs@gmail.com>
2023-04-06 23:13:07 +0200

    feat: char amounts are plotted in a histogram

---
+++ src/hist.py
@@ -0,0 +1,31 @@
+import json
+from matplotlib import pyplot as plt
+
+
+def main():
+    file_path = './char_count.json'
+
+    f = open(file_path)
+    data = json.load(f)
+    f.close()
+
+    fig, ax = plt.subplots(figsize =(10, 7))
+
+    ticks = [i for i in range(len(data.keys()))]
+    ticks_labels = list(data.keys())
+    y = list(data.values())
+
+    ax.bar(ticks_labels, y)
+
+    # print(ticks)
+    # print(ticks_labels)
+
+    ax.set_xticks(ticks)
+    ax.set_xticklabels(ticks_labels)
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
